58bfb949f77a7f62692be87bcef5ec8ad5bb001b
alexmgee <agee2@huskers.unl.edu>
2026-03-24 03:23:17 -0500

    feat: gate CLI behind distribution flag

--- prep360/__main__.py
+++ prep360/__main__.py
@@ -1,7 +1,9 @@
-"""
-Allow running prep360 as a module: python -m prep360
-"""
-from .cli import main
+import sys
 
 if __name__ == "__main__":
-    exit(main())
+    from prep360.distribution import is_gumroad
+    if is_gumroad():
+        print("CLI is not available in this build. Use the GUI instead.", file=sys.stderr)
+        sys.exit(1)
+    from .cli import main
+    sys.exit(main())
